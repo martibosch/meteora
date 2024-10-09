4ac8a748168aee6202b3f27be01dfe017910fe18
Martí Bosch <marti.bosch@protonmail.com>
2024-10-09 08:39:31 +0200

    docs: added github link in html theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,9 @@ extensions = [
 
 autodoc_typehints = "description"
 html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/martibosch/meteora",
+}
 
 # https://myst-parser.readthedocs.io/en/stable/syntax/optional.html#auto-generated-header-anchors
 myst_heading_anchors = 3
