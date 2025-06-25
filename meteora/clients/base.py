6f070860bb6615e2d125874e5303e163a731d7ad
Martí Bosch <marti.bosch@protonmail.com>
2025-06-25 14:39:28 +0200

    refactor: stations_gdf in BaseClient with _stations_gdf_id_col attr

--- meteora/clients/base.py
+++ meteora/clients/base.py
@@ -7,6 +7,7 @@ import re
 import time
 from collections.abc import Mapping
 
+import geopandas as gpd
 import pandas as pd
 import pyproj
 import requests
@@ -51,6 +52,11 @@ class BaseClient(RegionMixin, abc.ABC):
     @utils.abstract_attribute
     def CRS(self) -> pyproj.CRS:  # pylint: disable=invalid-name
         """CRS of the data source."""
+        pass
+
+    @utils.abstract_attribute
+    def _stations_gdf_id_col(self) -> str:
+        """Column with the station IDs in the `stations_gdf` returned by the API."""
         pass
 
     @utils.abstract_attribute
@@ -193,6 +199,47 @@ class BaseClient(RegionMixin, abc.ABC):
 
         return response_content
 
+    # stations
+    def _get_stations_gdf(self) -> gpd.GeoDataFrame:
+        """Get a GeoDataFrame featuring the stations data for the given region.
+
+        Returns
+        -------
+        stations_gdf : gpd.GeoDataFrame
+            The stations data for the given region as a GeoDataFrame.
+
+        """
+        stations_df = self._get_stations_df()
+        stations_gdf = gpd.GeoDataFrame(
+            stations_df,
+            geometry=gpd.points_from_xy(
+                stations_df[self.X_COL], stations_df[self.Y_COL]
+            ),
+            crs=self.CRS,
+        )
+        # filter the stations
+        # TODO: do we need to copy the dict to avoid reference issues?
+        _sjoin_kwargs = self.SJOIN_KWARGS.copy()
+        # predicate = _sjoin_kws.pop("predicate", SJOIN_PREDICATE)
+        return stations_gdf.sjoin(self.region[["geometry"]], **_sjoin_kwargs)[
+            stations_gdf.columns
+        ]
+
+    @property
+    def stations_gdf(self) -> gpd.GeoDataFrame:
+        """Geo-data frame with stations data."""
+        try:
+            return self._stations_gdf
+        except AttributeError:
+            # rename here because some clients may override `_get_stations_gdf`
+            self._stations_gdf = (
+                self._get_stations_gdf()
+                .rename(columns={self._stations_gdf_id_col: settings.STATIONS_ID_COL})
+                .set_index(settings.STATIONS_ID_COL)
+            )
+            return self._stations_gdf
+
+    # time series data
     @utils.abstract_attribute
     def _ts_endpoint(self) -> str:
         pass
