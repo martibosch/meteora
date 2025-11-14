47f40b6a3b1e40a76e45be84afb493eb8f856bab
Martí Bosch <marti.bosch@protonmail.com>
2025-11-14 11:31:01 +0100

    feat!: remove metoffice client (discontinued points api service)

--- meteora/clients/__init__.py
+++ meteora/clients/__init__.py
@@ -6,6 +6,5 @@ from meteora.clients.awel import AWELClient
 from meteora.clients.iem import ASOSOneMinIEMClient, METARASOSIEMClient
 from meteora.clients.meteocat import MeteocatClient
 from meteora.clients.meteoswiss import MeteoSwissClient
-from meteora.clients.metoffice import MetOfficeClient
 from meteora.clients.netatmo import NetatmoClient
 from meteora.clients.noaa import GHCNHourlyClient
